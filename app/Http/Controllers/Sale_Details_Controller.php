808691fffda281030d7947b8b229b8aa6ccc55f8
Prathamesh Varale <prathameshvarale07192@gmail.com>
2023-09-05 16:16:08 +0530

    Sales api created

---
+++ app/Http/Controllers/Sale_Details_Controller.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sale_Details_Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class Sale_Details_Controller extends Controller
+{
+    //
+
+     // Add Products Method
+
+     public function addSaleEntry (Request $request)
+     {
+         $product = new Sale_Details_Model;
+ 
+         $product->invoice_no = $request->input("invoice_no");
+         $product->date = $request->input("date");
+         $product->cust_id = $request->input("cust_id");
+         $product->cust_name = $request->input("cust_name");
+         $product->mobile = $request->input("mobile");
+         $product->gstin = $request->input("gstin");
+         $product->address = $request->input("address");
+         $product->place_of_supply = $request->input("place_of_supply");
+         $product->dispatch_no = $request->input("dispatch_no");
+         $product->destination = $request->input("destination");
+         $product->trans_amt = $request->input("trans_amt");
+         $product->hamali = $request->input("hamali");
+         $product->cgst = $request->input("cgst");
+         $product->cgst_amt = $request->input("cgst_amt");
+         $product->sgat = $request->input("sgat");
+         $product->sgat_amt = $request->input("sgat_amt");
+         $product->igst = $request->input("igst");
+         $product->igst_amt = $request->input("igst_amt");
+         $product->total = $request->input("total");
+
+         $product->save();
+ 
+         return response()->json(["message" => "Data saved successfully","status" => "success"],201);
+     }
+ 
+
+     // Fetch All Product data
+     public function fetchAllProducts()
+     {
+         $product = DB::select("SELECT * FROM tbl_sales_details where status = 1");
+     
+         if ($product) {
+             return response()->json(["data" => $product], 200);
+         } else {
+             return response()->json(["message" => "No data found"], 404);
+         }
+     }
+ 
+ 
+     // Fetch product by Id
+ 
+     public function fetchById($invoice_no)
+     {
+        try {
+            $product = DB::table('tbl_sales_details')
+                ->where('invoice_no', '=', $invoice_no)
+                ->first();
+        
+            if ($product) {
+                return response()->json(["data" => $product], 200);
+            } else {
+                return response()->json(["message" => "No data found"], 404);
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(["error" => "An error occurred"], 500);
+        }
+     }
+ 
+   
+ 
+   
+ 
+ 
+     
+}
---
+++ app/Http/Controllers/Sales_Product_Controller.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sales_Product_Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class Sales_Product_Controller extends Controller
+{
+    //
+     // Add Products Method
+
+     public function addProduct (Request $request)
+     {
+         $product = new Sales_Product_Model;
+         $product->sales_prod_id = $request->input("sales_prod_id");
+         $product->invoice_no = $request->input("invoice_no");
+         $product->s_date = $request->input("s_date");
+         $product->gp_id = $request->input("gp_id");
+         $product->prod_name = $request->input("prod_name");
+         $product->hsn = $request->input("hsn");
+         $product->weight = $request->input("weight");
+         $product->qty = $request->input("qty");
+         $product->rate = $request->input("rate");
+         $product->total = $request->input("total");
+         $product->type = $request->input("type");
+         $product->save();
+ 
+         return response()->json(["message" => "Data saved successfully","status" => "success"],201);
+     }
+ 
+
+     // Fetch All Product data
+     public function fetchAllProducts()
+     {
+         $product = DB::select("SELECT * FROM tbl_sales_product");
+     
+         if ($product) {
+             return response()->json(["data" => $product], 200);
+         } else {
+             return response()->json(["message" => "No data found"], 404);
+         }
+     }
+ 
+ 
+     // Fetch product by Id
+ 
+     public function fetchById($invoice_no)
+     {
+    
+         try {
+            $product = DB::table('tbl_sales_product')
+                ->where('invoice_no', '=', $invoice_no)
+                ->first();
+        
+            if ($product) {
+                return response()->json(["data" => $product], 200);
+            } else {
+                return response()->json(["message" => "No data found"], 404);
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(["error" => "An error occurred"], 500);
+        }
+     }
+ 
+ 
+    
+}
